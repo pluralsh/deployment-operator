957bde0e71dad665e53f7fcbff43fc2f0e10b0a9
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-09-15 13:25:48 +0200

    Update types

--- apis/deployment/v1alpha1/types.go
+++ apis/deployment/v1alpha1/types.go
@@ -21,13 +21,44 @@ type Application struct {
 }
 
 type ApplicationSpec struct {
+	// PluralId is an ID of deployment from Plural.
+	PluralId string `json:"pluralId"`
+
+	// RepoUrl is a URL of repository to sync.
+	RepoUrl string `json:"repoUrl"`
+
+	// Ref to fetch from the repository.
+	Ref string `json:"ref"`
+
+	// Subfolder from the repository to work with.
+	Subfolder string `json:"subfolder"`
+
+	// Namespace to sync into.
+	Namespace string `json:"namespace"`
 }
 
 type ApplicationStatus struct {
+	// Ref shows current ref its synced to.
+	Ref string `json:"ref"`
+
+	Resources []ApplicationResource `json:"resources"`
 }
 
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ApplicationResource struct {
+	APIVersion string                    `json:"apiVersion"`
+	Kind       string                    `json:"kind"`
+	Name       string                    `json:"name"`
+	Namespace  string                    `json:"namespace"`
+	Synced     bool                      `json:"synced"`
+	Status     ApplicationResourceStatus `json:"status"`
+}
 
+// ApplicationResourceStatus represents current status of application resource.
+// +kubebuilder:validation:Enum=Pending;Failed;Succeeded
+type ApplicationResourceStatus string
+
+// ApplicationList is a list of Applications.
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type ApplicationList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
