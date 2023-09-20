6870c7412164e08a4d497d084874be8d902e0375
Lukasz Zajaczkowski <zreigz@gmail.com>
2023-09-20 16:42:52 +0200

    improve argocd interface

--- providers/argocd/pkg/argocd/argocd.go
+++ providers/argocd/pkg/argocd/argocd.go
@@ -6,59 +6,24 @@ import (
 	"github.com/argoproj/argo-cd/v2/pkg/apiclient"
 	applicationpkg "github.com/argoproj/argo-cd/v2/pkg/apiclient/application"
 	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type Argocd struct {
 	apiclient.Client
 }
 
-func (c *Argocd) CreateApplication(ctx context.Context, name string) (*v1alpha1.Application, error) {
+func (c *Argocd) CreateApplication(ctx context.Context, application *v1alpha1.Application) (*v1alpha1.Application, error) {
 	closer, appClient, err := c.NewApplicationClient()
 	if err != nil {
 		return nil, err
 	}
 	defer closer.Close()
-	return appClient.Create(context.Background(), &applicationpkg.ApplicationCreateRequest{
-		Application: &v1alpha1.Application{
-			TypeMeta: metav1.TypeMeta{},
-			ObjectMeta: metav1.ObjectMeta{
-				Name: name,
-			},
-			Spec: v1alpha1.ApplicationSpec{
-				Source: &v1alpha1.ApplicationSource{
-					RepoURL:        "",
-					Path:           "",
-					TargetRevision: "",
-					Helm:           nil,
-					Kustomize:      nil,
-					Directory:      nil,
-					Plugin:         nil,
-					Chart:          "",
-					Ref:            "",
-				},
-				Destination: v1alpha1.ApplicationDestination{
-					Server:    "",
-					Namespace: "",
-					Name:      "",
-				},
-				Project:              "",
-				SyncPolicy:           &v1alpha1.SyncPolicy{},
-				IgnoreDifferences:    nil,
-				Info:                 nil,
-				RevisionHistoryLimit: nil,
-				Sources:              v1alpha1.ApplicationSources{},
-			},
-			Status: v1alpha1.ApplicationStatus{},
-			Operation: &v1alpha1.Operation{
-				Sync:        nil,
-				InitiatedBy: v1alpha1.OperationInitiator{},
-				Info:        nil,
-				Retry:       v1alpha1.RetryStrategy{},
-			},
-		},
-		Upsert:   nil,
-		Validate: nil,
+	upsert := true
+	validate := true
+	return appClient.Create(ctx, &applicationpkg.ApplicationCreateRequest{
+		Application: application,
+		Upsert:      &upsert,
+		Validate:    &validate,
 	})
 }
 
@@ -80,14 +45,16 @@ func (c *Argocd) DeleteApplication(ctx context.Context, namespace, name, project
 	return nil
 }
 
-func (c *Argocd) GetApplicationStatus(ctx context.Context, name string) (*v1alpha1.ApplicationStatus, error) {
+func (c *Argocd) GetApplicationStatus(ctx context.Context, namespace, name, project string) (*v1alpha1.ApplicationStatus, error) {
 	closer, appClient, err := c.NewApplicationClient()
 	if err != nil {
 		return nil, err
 	}
 	defer closer.Close()
 	app, err := appClient.Get(ctx, &applicationpkg.ApplicationQuery{
-		Name: &name,
+		Name:         &name,
+		Project:      []string{project},
+		AppNamespace: &namespace,
 	})
 	if err != nil {
 		return nil, err
