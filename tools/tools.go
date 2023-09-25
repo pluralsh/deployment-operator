cfb5b43a6ebb6dfab3c13046bd6c97145d142ae5
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-09-25 12:22:31 +0200

    add protoc-gen-go-grpc and protoc-gen-go to tools

--- tools/tools.go
+++ tools/tools.go
@@ -4,5 +4,7 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
