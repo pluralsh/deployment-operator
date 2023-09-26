0c06a59f128d3073d0a835b62569a382664cf2dd
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-09-26 09:55:34 +0200

    remove other leftovers

--- tools/tools.go
+++ tools/tools.go
@@ -4,7 +4,4 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
