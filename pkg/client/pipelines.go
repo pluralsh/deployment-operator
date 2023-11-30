e8ff341562ded9edc67a25a33b9f87e402ec1b32
Hans Rauer <h.w.rauer@googlemail.com>
2023-11-30 17:17:21 +0100

    rm generated files causing compiler issues

--- pkg/client/pipelines.go
+++ pkg/client/pipelines.go
@@ -1,12 +1,13 @@
 package client
 
 import (
+	"fmt"
+
 	console "github.com/pluralsh/console-client-go"
 	pipelinesv1alpha1 "github.com/pluralsh/deployment-operator/apis/pipelines/v1alpha1"
 	batchv1 "k8s.io/api/batch/v1"
-	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
 )
 
 func (c *Client) UpdateGate(id string, attributes console.GateUpdateAttributes) error {
@@ -104,77 +105,112 @@ func (c *Client) ParsePipelineGateCR(fragment *console.PipelineGateFragment) (*p
 			APIVersion: "yourgroup/version",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: fragment.Name,
+			Name:      fragment.Name,
+			Namespace: fragment.Spec.Job.Namespace,
 			// You may set other metadata fields as needed (namespace, labels, annotations)
 		},
 		Spec: pipelinesv1alpha1.PipelineGateSpec{
-			ID:   fragment.ID,
-			Name: fragment.Name,
-			Type: fragment.Type,
-			GateSpec: GateSpec{
-				JobSpec: convertToJobSpec(fragment.Spec.Job),
-			},
+			ID:       fragment.ID,
+			Name:     fragment.Name,
+			Type:     pipelinesv1alpha1.GateType(fragment.Type),
+			GateSpec: pipelinesv1alpha1.GateSpec{convertToJobSpec(&fragment.Spec.Job)},
 		},
-		Status: PipelineGateStatus{
-			State: fragment.State,
+		Status: pipelinesv1alpha1.PipelineGateStatus{
+			State: pipelinesv1alpha1.GateState(fragment.State),
 		},
 	}
 
 	return pipelineGate, nil
 }
 
-// Helper function to convert the GateSpecFragment.Job into batchv1.JobSpec
-func convertToJobSpec(jobFragment *console.GateSpecFragmentJob) batchv1.JobSpec {
-	if jobFragment == nil {
+func parseYAMLIntoJob(yamlString string) (*batchv1.Job, error) {
+	job := &batchv1.Job{}
+
+	// unmarshal the YAML string into the Job rep
+	decoder := scheme.Codecs.UniversalDeserializer()
+	obj, _, err := decoder.Decode([]byte(yamlString), nil, job)
+	if err != nil {
+		return nil, err
+	}
+
+	// ensure decoded object is actually of type Job after using universal deserializer
+	if obj, ok := obj.(*batchv1.Job); ok {
+		return obj, nil
+	}
+
+	return nil, fmt.Errorf("parsed object is not of type JobSpec")
+}
+
+// //////////////// Helper function to convert the GateSpecFragment.Job into batchv1.JobSpec
+func convertToJobSpec(fragment *console.JobSpecFragment) (batchv1.JobSpec, error) {
+	//type JobSpecFragment struct {
+	//	Namespace      string                   "json:\"namespace\" graphql:\"namespace\""
+	//	Raw            *string                  "json:\"raw\" graphql:\"raw\""
+	//	Containers     []*ContainerSpecFragment "json:\"containers\" graphql:\"containers\""
+	//	Labels         map[string]interface{}   "json:\"labels\" graphql:\"labels\""
+	//	Annotations    map[string]interface{}   "json:\"annotations\" graphql:\"annotations\""
+	//	ServiceAccount *string                  "json:\"serviceAccount\" graphql:\"serviceAccount\""
+	//}
+	if fragment == nil {
 		return batchv1.JobSpec{}
 	}
 
-	// Create a basic JobSpec
-	jobSpec := batchv1.JobSpec{
-		Template: batchv1.PodTemplateSpec{
-			Spec: batchv1.PodSpec{
-				Containers: convertToContainers(jobFragment.Containers),
-				Labels:     jobFragment.Labels,
-				// Set other fields as needed
-			},
-		},
-		// Set other fields as needed
+	if fragment.Raw != nil {
+		job := parseYAMLIntoJob(*fragment.Raw)
+		return parseYAMLIntoJob(*fragment.Raw)
 	}
+
+	// Create a basic JobSpec
+	//jobSpec := batchv1.JobSpec{
+	//	metav1.ObjectMeta{
+	//		Name:      fragment.Name,
+	//		Namespace: fragment.Namespace,
+	//	},
+	//	Template: corev1.PodTemplateSpec{
+	//		Spec: corev1.PodSpec{
+	//			Containers: convertToContainers(fragment.Containers),
+	//			Labels:     fragment.Labels,
+	//			// Set other fields as needed
+	//		},
+	//	},
+	//	// Set other fields as needed
+	//}
 
 	return jobSpec
 }
 
-// Helper function to convert []*console.GateSpecFragmentJobContainer into []v1.Container
-func convertToContainers(containerFragments []*console.GateSpecFragmentJobContainer) []v1.Container {
-	var containers []v1.Container
-
-	for _, containerFragment := range containerFragments {
-		container := v1.Container{
-			Name:  containerFragment.Image,
-			Image: containerFragment.Image,
-			Args:  containerFragment.Args,
-			Env:   convertToEnvVars(containerFragment.Env),
-			// Set other fields as needed
-		}
-
-		containers = append(containers, container)
-	}
-
-	return containers
-}
-
-// Helper function to convert []*console.GateSpecFragmentJobContainerEnv into []v1.EnvVar
-func convertToEnvVars(envFragments []*console.GateSpecFragmentJobContainerEnv) []v1.EnvVar {
-	var envVars []corev1.EnvVar
-
-	for _, envFragment := range envFragments {
-		envVar := corev1.EnvVar{
-			Name:  envFragment.Name,
-			Value: envFragment.Value,
-		}
-
-		envVars = append(envVars, envVar)
-	}
-
-	return envVars
-}
+//
+//// Helper function to convert []*console.GateSpecFragmentJobContainer into []v1.Container
+//func convertToContainers(*console.GateSpecFragment) []v1.Container {
+//	var containers []v1.Container
+//
+//	for _, containerFragment := range containerFragments {
+//		container := v1.Container{
+//			Name:  containerFragment.Image,
+//			Image: containerFragment.Image,
+//			Args:  containerFragment.Args,
+//			Env:   convertToEnvVars(containerFragment.Env),
+//			// Set other fields as needed
+//		}
+//
+//		containers = append(containers, container)
+//	}
+//
+//	return containers
+//}
+//
+//// Helper function to convert []*console.GateSpecFragmentJobContainerEnv into []v1.EnvVar
+//func convertToEnvVars(envFragments []*console.GateSpecFragment) []v1.EnvVar {
+//	var envVars []corev1.EnvVar
+//
+//	for _, envFragment := range envFragments {
+//		envVar := corev1.EnvVar{
+//			Name:  envFragment.Name,
+//			Value: envFragment.Value,
+//		}
+//
+//		envVars = append(envVars, envVar)
+//	}
+//
+//	return envVars
+//}
