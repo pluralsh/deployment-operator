d84ff282aa64e64eee2ba780e0d3785eadb170ab
Hans Rauer <h.w.rauer@googlemail.com>
2023-12-14 17:46:15 +0100

    rm comments

--- apis/pipelines/v1alpha1/pipelinegate_types.go
+++ apis/pipelines/v1alpha1/pipelinegate_types.go
@@ -24,45 +24,28 @@ import (
 )
 
 // +kubebuilder:validation:Enum=PENDING;OPEN;CLOSED
-// GateState represents the state of a gate
-// type GateState string
+// GateState represents the state of a gate, reused from console client
 type GateState console.GateState
 
-//const (
-//	Pending GateState = "PENDING"
-//	Open    GateState = "OPEN"
-//	Closed  GateState = "CLOSED"
-//)
-
 // +kubebuilder:validation:Enum=APPROVAL;WINDOW;JOB
-// GateType represents the type of a gate
-// type GateType string
+// GateType represents the type of a gate, reused from console client
 type GateType console.GateType
-
-//const (
-//	Approval GateType = "APPROVAL"
-//	Window   GateType = "WINDOW"
-//	Job      GateType = "JOB"
-//)
 
 //+genclient
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
-// PipelineGate is the Schema for the pipelinegate API
 // PipelineGate represents a gate blocking promotion along a release pipeline
 type PipelineGate struct {
-	metav1.TypeMeta   `json:",inline"`            // kind and apiVersion
-	metav1.ObjectMeta `json:"metadata,omitempty"` // name, namespace, labels, annotations
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Spec   PipelineGateSpec   `json:"spec,omitempty"`
 	Status PipelineGateStatus `json:"status,omitempty"`
 }
 
-// PipelineGateStatus defines the observed state of ConfigurationOverlay
+// PipelineGateStatus defines the observed state of the PipelineGate
 type PipelineGateStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 	State GateState `json:"state"`
 }
 
@@ -76,40 +59,9 @@ type PipelineGateSpec struct {
 
 // GateSpec defines the detailed gate specifications
 type GateSpec struct {
-	// TODO: think about replacing this with the actual type from the k8s api
+	// resuse JobSpec type from the kubernetes api
 	JobSpec *batchv1.JobSpec `json:"job"`
 }
-
-//// JobGateSpec represents the full specification of a job gate
-//type JobGateSpec struct {
-//	Namespace  string          `json:"namespace"`
-//	Raw        string          `json:"raw"`
-//	Containers []ContainerSpec `json:"containers"`
-//	// TODO: add support for these
-//	//Labels         Map             `json:"labels"`
-//	//Annotations    Map             `json:"annotations"`
-//	ServiceAccount string `json:"serviceAccount"`
-//}
-//
-//// ContainerSpec represents a shortform spec for job containers, designed for ease-of-use
-//type ContainerSpec struct {
-//	Image   string             `json:"image"`
-//	Args    []string           `json:"args"`
-//	Env     []ContainerEnv     `json:"env"`
-//	EnvFrom []ContainerEnvFrom `json:"envFrom"`
-//}
-//
-//// ContainerEnv represents a container env variable
-//type ContainerEnv struct {
-//	Name  string `json:"name"`
-//	Value string `json:"value"`
-//}
-//
-//// ContainerEnvFrom represents env from declarations for containers
-//type ContainerEnvFrom struct {
-//	ConfigMap string `json:"configMap"`
-//	Secret    string `json:"secret"`
-//}
 
 // +kubebuilder:object:root=true
 // PipelineGateList contains a list of PipelineGate
