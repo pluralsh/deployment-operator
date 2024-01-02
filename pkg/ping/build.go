9aaee96293bf235023b711acccc0e51f7e4423be
michaeljguarino <mguarino46@gmail.com>
2024-01-02 10:09:35 -0500

    Extended Ping (#100)
    Adds the ability to detect cluster distribution and return that info in ping requests.  Also refactor pinging into its own package as the logic might become more expansive as this evolves.

---
+++ pkg/ping/build.go
@@ -0,0 +1,17 @@
+package ping
+
+import (
+	"strings"
+
+	console "github.com/pluralsh/console-client-go"
+	"github.com/samber/lo"
+	"k8s.io/apimachinery/pkg/version"
+)
+
+func pingAttributes(info *version.Info, pods []string) console.ClusterPing {
+	vs := strings.Split(info.GitVersion, "-")
+	return console.ClusterPing{
+		CurrentVersion: strings.TrimPrefix(vs[0], "v"),
+		Distro:         lo.ToPtr(findDistro(append(pods, info.GitVersion))),
+	}
+}
