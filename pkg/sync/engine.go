f43b25388d9aab58051ac13439b03b630f1c72c2
Hans Rauer <h.w.rauer@googlemail.com>
2024-01-14 14:41:57 +0100

    engine changes

--- pkg/sync/engine.go
+++ pkg/sync/engine.go
@@ -9,6 +9,9 @@ import (
 
 	"time"
 
+	generated "github.com/pluralsh/deployment-operator/generated/client/clientset/versioned"
+	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
+
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/kubectl/pkg/cmd/util"
 	"sigs.k8s.io/cli-utils/pkg/apply"
@@ -18,6 +21,8 @@ import (
 type Engine struct {
 	client            *client.Client
 	clientset         *kubernetes.Clientset
+	genClientset      *generated.Clientset
+	ctrlClient        ctrlclient.Client
 	svcQueue          workqueue.RateLimitingInterface
 	deathChan         chan interface{}
 	svcCache          *client.ServiceCache
@@ -28,11 +33,27 @@ type Engine struct {
 	destroyer         *apply.Destroyer
 	utilFactory       util.Factory
 	processingTimeout time.Duration
+	gateQueue         workqueue.RateLimitingInterface
 }
 
-func New(utilFactory util.Factory, invFactory inventory.ClientFactory, applier *applier.Applier, destroyer *apply.Destroyer, client *client.Client, svcQueue workqueue.RateLimitingInterface, svcCache *client.ServiceCache, manCache *manifests.ManifestCache, processingTimeout time.Duration) *Engine {
+func New(
+	utilFactory util.Factory,
+	invFactory inventory.ClientFactory,
+	applier *applier.Applier,
+	destroyer *apply.Destroyer,
+	client *client.Client,
+	ctrlclient ctrlclient.Client,
+	genClientset *generated.Clientset,
+	svcQueue workqueue.RateLimitingInterface,
+	svcCache *client.ServiceCache,
+	manCache *manifests.ManifestCache,
+	processingTimeout time.Duration,
+	gateQueue workqueue.RateLimitingInterface,
+) *Engine {
 	return &Engine{
 		client:            client,
+		ctrlClient:        ctrlclient,
+		genClientset:      genClientset,
 		svcQueue:          svcQueue,
 		svcCache:          svcCache,
 		manifestCache:     manCache,
@@ -41,6 +62,7 @@ func New(utilFactory util.Factory, invFactory inventory.ClientFactory, applier *
 		destroyer:         destroyer,
 		utilFactory:       utilFactory,
 		processingTimeout: processingTimeout,
+		gateQueue:         gateQueue,
 	}
 }
 
