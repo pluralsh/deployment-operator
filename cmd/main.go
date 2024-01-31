3b856544ed76f93d2784dd4b1d3cc7b9fac99fa8
Hans Rauer <h.w.rauer@googlemail.com>
2024-01-31 14:48:20 +0100

    adapt to new format

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,6 @@ import (
 	deploymentsv1alpha1 "github.com/pluralsh/deployment-operator/api/v1alpha1"
 	pipelinecontroller "github.com/pluralsh/deployment-operator/controllers/pipelines"
 	"github.com/pluralsh/deployment-operator/internal/controller"
-	"github.com/pluralsh/deployment-operator/pkg/agent"
 	"github.com/pluralsh/deployment-operator/pkg/client"
 	"github.com/pluralsh/deployment-operator/pkg/log"
 	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
@@ -40,7 +39,7 @@ func main() {
 	config := ctrl.GetConfigOrDie()
 	ctx := ctrl.SetupSignalHandler()
 
-	ctrlMgr, serviceReconciler := runAgent(opt, config, ctx)
+	_, serviceReconciler := runAgent(opt, config, ctx)
 
 	mgr, err := ctrl.NewManager(config, ctrl.Options{
 		Scheme:                 scheme,
@@ -53,20 +52,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controller.RestoreReconciler{
-		Client:        mgr.GetClient(),
-		Scheme:        mgr.GetScheme(),
-		ConsoleClient: ctrlMgr.GetClient(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Restore")
-	}
-	if err = (&controller.ClusterBackupReconciler{
-		Client:        mgr.GetClient(),
-		Scheme:        mgr.GetScheme(),
-		ConsoleClient: ctrlMgr.GetClient(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Backup")
-	}
+	//if err = (&controller.RestoreReconciler{
+	//	Client:        mgr.GetClient(),
+	//	Scheme:        mgr.GetScheme(),
+	//	ConsoleClient: ctrlMgr.GetClient(),
+	//}).SetupWithManager(mgr); err != nil {
+	//	setupLog.Error(err, "unable to create controller", "controller", "Restore")
+	//}
+	//if err = (&controller.ClusterBackupReconciler{
+	//	Client:        mgr.GetClient(),
+	//	Scheme:        mgr.GetScheme(),
+	//	ConsoleClient: ctrlMgr.GetClient(),
+	//}).SetupWithManager(mgr); err != nil {
+	//	setupLog.Error(err, "unable to create controller", "controller", "Backup")
+	//}
 
 	if err = (&controller.CustomHealthReconciler{
 		Client:            mgr.GetClient(),
@@ -77,20 +76,6 @@ func main() {
 	}
 	//+kubebuilder:scaffold:builder
 
-	if err = mgr.AddHealthzCheck("ping", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to create health check")
-		os.Exit(1)
-	}
-
-	a, err := agent.New(mgr, refresh, pTimeout, opt.consoleUrl, opt.deployToken, opt.clusterId)
-	if err != nil {
-		setupLog.Error(err, "unable to create agent")
-		os.Exit(1)
-	}
-	if err := a.SetupWithManager(); err != nil {
-		setupLog.Error(err, "unable to start agent")
-		os.Exit(1)
-	}
 	if err = (&pipelinecontroller.PipelineGateReconciler{
 		Client:        mgr.GetClient(),
 		ConsoleClient: client.New(opt.consoleUrl, opt.deployToken),
@@ -101,7 +86,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	ctx := ctrl.SetupSignalHandler()
+	if err = mgr.AddHealthzCheck("ping", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to create health check")
+		os.Exit(1)
+	}
+
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
