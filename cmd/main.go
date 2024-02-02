bb92a1e7f53a5689d5e381ead8c5d908fcf6c9f1
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-02-02 12:32:13 +0100

    enable backup reconciler

--- cmd/main.go
+++ cmd/main.go
@@ -43,15 +43,15 @@ func main() {
 	}
 
 	setupLog.Info("starting agent")
-	_, serviceReconciler := runAgent(opt, config, ctx, mgr.GetClient())
+	ctrlMgr, serviceReconciler := runAgent(opt, config, ctx, mgr.GetClient())
 
-	//if err = (&controller.BackupReconciler{
-	//	Client:        mgr.GetClient(),
-	//	Scheme:        mgr.GetScheme(),
-	//	ConsoleClient: ctrlMgr.GetClient(),
-	//}).SetupWithManager(mgr); err != nil {
-	//	setupLog.Error(err, "unable to create controller", "controller", "Backup")
-	//}
+	if err = (&controller.BackupReconciler{
+		Client:        mgr.GetClient(),
+		Scheme:        mgr.GetScheme(),
+		ConsoleClient: ctrlMgr.GetClient(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Backup")
+	}
 
 	if err = (&controller.CustomHealthReconciler{
 		Client:            mgr.GetClient(),
