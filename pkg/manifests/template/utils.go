a97d7fd269977a14b050428e2ef275bbc82aa068
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-02-13 10:34:51 +0100

    service context templating

--- pkg/manifests/template/utils.go
+++ pkg/manifests/template/utils.go
@@ -12,3 +12,11 @@ func configMap(svc *console.GetServiceDeploymentForAgent_ServiceDeployment) map[
 
 	return res
 }
+
+func contexts(svc *console.GetServiceDeploymentForAgent_ServiceDeployment) map[string]map[string]interface{} {
+	res := map[string]map[string]interface{}{}
+	for _, context := range svc.Contexts {
+		res[context.Name] = context.Configuration
+	}
+	return res
+}
