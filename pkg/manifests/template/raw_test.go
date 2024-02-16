ac5931979730fcd6d422f42433c8e56df5e10483
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-02-16 14:45:09 +0100

    support templated flag

--- pkg/manifests/template/raw_test.go
+++ pkg/manifests/template/raw_test.go
@@ -1,6 +1,7 @@
 package template
 
 import (
+	"github.com/samber/lo"
 	"path/filepath"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -10,7 +11,6 @@ import (
 
 var _ = Describe("Raw template", func() {
 
-	dir := filepath.Join("..", "..", "..", "test", "raw")
 	svc := &console.GetServiceDeploymentForAgent_ServiceDeployment{
 		Namespace:     "default",
 		Configuration: make([]*console.GetServiceDeploymentForAgent_ServiceDeployment_Configuration, 0),
@@ -18,12 +18,21 @@ var _ = Describe("Raw template", func() {
 	Context("Render raw template", func() {
 		const name = "nginx"
 		It("should successfully render the raw template", func() {
+			dir := filepath.Join("..", "..", "..", "test", "raw")
 			svc.Configuration = []*console.GetServiceDeploymentForAgent_ServiceDeployment_Configuration{
 				{
 					Name:  "name",
 					Value: name,
 				},
 			}
+			resp, err := NewRaw(dir).Render(svc, utilFactory)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(resp)).To(Equal(1))
+			Expect(resp[0].GetName()).To(Equal(name))
+		})
+		It("should skip templating liquid", func() {
+			dir := filepath.Join("..", "..", "..", "test", "rawTemplated")
+			svc.Templated = lo.ToPtr(false)
 			resp, err := NewRaw(dir).Render(svc, utilFactory)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(len(resp)).To(Equal(1))
