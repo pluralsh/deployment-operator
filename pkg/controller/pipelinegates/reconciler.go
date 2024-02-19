6ab56abd0a1b06e11a6d594122ce1f8e7474eec8
michaeljguarino <mguarino46@gmail.com>
2024-02-19 02:55:44 -0500

    Reduce some unnecessary logging (#125)
    Some debug messages are being emitted that don't necessarily need to be in the production build of the agent.

--- pkg/controller/pipelinegates/reconciler.go
+++ pkg/controller/pipelinegates/reconciler.go
@@ -112,7 +112,7 @@ func (s *GateReconciler) Poll(ctx context.Context) (done bool, err error) {
 		after = resp.PagedClusterGates.PageInfo.EndCursor
 
 		for _, gate := range resp.PagedClusterGates.Edges {
-			logger.Info("sending update for", "gate", gate.Node.ID)
+			logger.V(2).Info("sending update for", "gate", gate.Node.ID)
 			s.GateQueue.Add(gate.Node.ID)
 		}
 	}
