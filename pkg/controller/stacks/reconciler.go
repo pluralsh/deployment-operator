69b23c121322cd0cf60836442c530d2850bedf05
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-04-24 11:49:10 +0200

    handle job

--- pkg/controller/stacks/reconciler.go
+++ pkg/controller/stacks/reconciler.go
@@ -11,6 +11,10 @@ import (
 	"github.com/pluralsh/deployment-operator/pkg/controller"
 	"github.com/pluralsh/deployment-operator/pkg/websocket"
 	"github.com/pluralsh/polly/algorithms"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -35,26 +39,26 @@ func NewStackReconciler(consoleClient client.Client, k8sClient ctrlclient.Client
 	}
 }
 
-func (n *StackReconciler) GetPublisher() (string, websocket.Publisher) {
+func (r *StackReconciler) GetPublisher() (string, websocket.Publisher) {
 	return "stack.event", &socketPublisher{
-		stackRunQueue: n.StackQueue,
-		stackRunCache: n.StackCache,
+		stackRunQueue: r.StackQueue,
+		stackRunCache: r.StackCache,
 	}
 }
 
-func (n *StackReconciler) WipeCache() {
-	n.StackCache.Wipe()
+func (r *StackReconciler) WipeCache() {
+	r.StackCache.Wipe()
 }
 
-func (n *StackReconciler) ShutdownQueue() {
-	n.StackQueue.ShutDown()
+func (r *StackReconciler) ShutdownQueue() {
+	r.StackQueue.ShutDown()
 }
 
-func (n *StackReconciler) ListStacks(ctx context.Context) *algorithms.Pager[*console.StackRunEdgeFragment] {
+func (r *StackReconciler) ListStacks(ctx context.Context) *algorithms.Pager[*console.StackRunEdgeFragment] {
 	logger := log.FromContext(ctx)
 	logger.Info("create stack run pager")
 	fetch := func(page *string, size int64) ([]*console.StackRunEdgeFragment, *algorithms.PageInfo, error) {
-		resp, err := n.ConsoleClient.ListClusterStackRuns(page, &size)
+		resp, err := r.ConsoleClient.ListClusterStackRuns(page, &size)
 		if err != nil {
 			logger.Error(err, "failed to fetch stack run")
 			return nil, nil, err
@@ -69,10 +73,10 @@ func (n *StackReconciler) ListStacks(ctx context.Context) *algorithms.Pager[*con
 	return algorithms.NewPager[*console.StackRunEdgeFragment](controller.DefaultPageSize, fetch)
 }
 
-func (n *StackReconciler) Poll(ctx context.Context) (done bool, err error) {
+func (r *StackReconciler) Poll(ctx context.Context) (done bool, err error) {
 	logger := log.FromContext(ctx)
 	logger.Info("fetching stacks")
-	pager := n.ListStacks(ctx)
+	pager := r.ListStacks(ctx)
 
 	for pager.HasNext() {
 		stacks, err := pager.NextPage()
@@ -82,17 +86,17 @@ func (n *StackReconciler) Poll(ctx context.Context) (done bool, err error) {
 		}
 		for _, stack := range stacks {
 			logger.Info("sending update for", "stack run", stack.Node.ID)
-			n.StackQueue.Add(stack.Node.ID)
+			r.StackQueue.Add(stack.Node.ID)
 		}
 	}
 
 	return false, nil
 }
 
-func (n *StackReconciler) Reconcile(ctx context.Context, id string) (reconcile.Result, error) {
+func (r *StackReconciler) Reconcile(ctx context.Context, id string) (reconcile.Result, error) {
 	logger := log.FromContext(ctx)
 	logger.Info("attempting to sync stack run", "id", id)
-	stackRun, err := n.StackCache.Get(id)
+	_, err := r.StackCache.Get(id)
 	if err != nil {
 		if clienterrors.IsNotFound(err) {
 			logger.Info("stack run already deleted", "id", id)
@@ -102,7 +106,62 @@ func (n *StackReconciler) Reconcile(ctx context.Context, id string) (reconcile.R
 		return reconcile.Result{}, err
 	}
 
-	fmt.Printf(stackRun.ID)
+	job := &batchv1.Job{}
+
+	reconciledJob, err := r.Job(ctx, job)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	// check job status
+	if hasFailed(reconciledJob) {
+
+	}
+	if hasSucceeded(reconciledJob) {
+
+	}
 
 	return reconcile.Result{}, nil
+}
+
+// Job reconciles a k8s job object.
+func (r *StackReconciler) Job(ctx context.Context, job *batchv1.Job) (*batchv1.Job, error) {
+	logger := log.FromContext(ctx)
+	foundJob := &batchv1.Job{}
+	if err := r.K8sClient.Get(ctx, types.NamespacedName{Name: job.Name, Namespace: job.Namespace}, foundJob); err != nil {
+		if !apierrs.IsNotFound(err) {
+			return nil, err
+		}
+		logger.V(2).Info("Creating Job.", "Namespace", job.Namespace, "Name", job.Name)
+		if err := r.K8sClient.Create(ctx, job); err != nil {
+			logger.Error(err, "Unable to create Job.")
+			return nil, err
+		}
+		return job, nil
+	}
+	return foundJob, nil
+
+}
+
+func hasFailed(job *batchv1.Job) bool {
+	return isJobStatusConditionTrue(job.Status.Conditions, batchv1.JobFailed)
+}
+
+func hasSucceeded(job *batchv1.Job) bool {
+	return isJobStatusConditionTrue(job.Status.Conditions, batchv1.JobComplete)
+}
+
+// isJobStatusConditionTrue returns true when the conditionType is present and set to `metav1.ConditionTrue`
+func isJobStatusConditionTrue(conditions []batchv1.JobCondition, conditionType batchv1.JobConditionType) bool {
+	return isJobStatusConditionPresentAndEqual(conditions, conditionType, corev1.ConditionTrue)
+}
+
+// isJobStatusConditionPresentAndEqual returns true when conditionType is present and equal to status.
+func isJobStatusConditionPresentAndEqual(conditions []batchv1.JobCondition, conditionType batchv1.JobConditionType, status corev1.ConditionStatus) bool {
+	for _, condition := range conditions {
+		if condition.Type == conditionType {
+			return condition.Status == status
+		}
+	}
+	return false
 }
