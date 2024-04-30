2943ca172646340c4581219df0d402787e7deb58
michaeljguarino <mguarino46@gmail.com>
2024-04-30 16:50:33 -0400

    fix: Custom implement ternary + default (#177)
    * Custom implement ternary + default
    
    Looks like the monkeypatching has some weird golang bug so just directly implement these
    
    * fix lint

--- pkg/manifests/template/utils.go
+++ pkg/manifests/template/utils.go
@@ -1,6 +1,7 @@
 package template
 
 import (
+	"reflect"
 	"strings"
 
 	console "github.com/pluralsh/console-client-go"
@@ -30,4 +31,47 @@ func indent(v string, spaces int) string {
 
 func nindent(v string, spaces int) string {
 	return "\n" + indent(v, spaces)
+}
+
+func ternary(v bool, vt interface{}, vf interface{}) interface{} {
+	if v {
+		return vt
+	}
+
+	return vf
+}
+
+func dfault(v1, v2 interface{}) interface{} {
+	if empty(v1) {
+		return v2
+	}
+
+	return v1
+}
+
+func empty(given interface{}) bool {
+	g := reflect.ValueOf(given)
+	if !g.IsValid() {
+		return true
+	}
+
+	// Basically adapted from text/template.isTrue
+	switch g.Kind() {
+	default:
+		return g.IsNil()
+	case reflect.Array, reflect.Slice, reflect.Map, reflect.String:
+		return g.Len() == 0
+	case reflect.Bool:
+		return !g.Bool()
+	case reflect.Complex64, reflect.Complex128:
+		return g.Complex() == 0
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return g.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return g.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return g.Float() == 0
+	case reflect.Struct:
+		return false
+	}
 }
