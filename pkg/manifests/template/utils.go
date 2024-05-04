f8dbc5cabea3c62ca45529ce424070173effa64b
michaeljguarino <mguarino46@gmail.com>
2024-05-04 14:09:29 -0400

    Use new polly implementation for liquid/tpl templates
    Also auto-infer kustomize services
    
    Fixes ENG-2044

--- pkg/manifests/template/utils.go
+++ pkg/manifests/template/utils.go
@@ -1,11 +1,39 @@
 package template
 
 import (
-	"reflect"
 	"strings"
 
 	console "github.com/pluralsh/console-client-go"
 )
+
+func isTemplated(svc *console.GetServiceDeploymentForAgent_ServiceDeployment) bool {
+	if svc.Templated != nil {
+		return *svc.Templated
+	}
+	// default true
+	return true
+}
+
+func clusterConfiguration(cluster *console.GetServiceDeploymentForAgent_ServiceDeployment_Cluster) map[string]interface{} {
+	res := map[string]interface{}{
+		"ID":             cluster.ID,
+		"Self":           cluster.Self,
+		"Handle":         cluster.Handle,
+		"Name":           cluster.Name,
+		"Version":        cluster.Version,
+		"CurrentVersion": cluster.CurrentVersion,
+		"KasUrl":         cluster.KasURL,
+		"Metadata":       cluster.Metadata,
+	}
+
+	for k, v := range res {
+		res[strings.ToLower(k)] = v
+	}
+	res["kasUrl"] = cluster.KasURL
+	res["currentVersion"] = cluster.CurrentVersion
+
+	return res
+}
 
 func configMap(svc *console.GetServiceDeploymentForAgent_ServiceDeployment) map[string]string {
 	res := map[string]string{}
@@ -22,56 +50,4 @@ func contexts(svc *console.GetServiceDeploymentForAgent_ServiceDeployment) map[s
 		res[context.Name] = context.Configuration
 	}
 	return res
-}
-
-func indent(v string, spaces int) string {
-	pad := strings.Repeat(" ", spaces)
-	return pad + strings.ReplaceAll(v, "\n", "\n"+pad)
-}
-
-func nindent(v string, spaces int) string {
-	return "\n" + indent(v, spaces)
-}
-
-func ternary(v bool, vt interface{}, vf interface{}) interface{} {
-	if v {
-		return vt
-	}
-
-	return vf
-}
-
-func dfault(v1, v2 interface{}) interface{} {
-	if empty(v1) {
-		return v2
-	}
-
-	return v1
-}
-
-func empty(given interface{}) bool {
-	g := reflect.ValueOf(given)
-	if !g.IsValid() {
-		return true
-	}
-
-	// Basically adapted from text/template.isTrue
-	switch g.Kind() {
-	default:
-		return g.IsNil()
-	case reflect.Array, reflect.Slice, reflect.Map, reflect.String:
-		return g.Len() == 0
-	case reflect.Bool:
-		return !g.Bool()
-	case reflect.Complex64, reflect.Complex128:
-		return g.Complex() == 0
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return g.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return g.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return g.Float() == 0
-	case reflect.Struct:
-		return false
-	}
 }
