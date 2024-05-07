77004d5cdeb2aaf54d1cc5cf8e1df414ddc6a4d4
Sebastian Florek <sebastian@plural.sh>
2024-05-07 15:01:59 +0200

    update gh action and fix gitignore

---
+++ pkg/harness/stackrun/types.go
@@ -0,0 +1,41 @@
+package stackrun
+
+import (
+	"fmt"
+
+	gqlclient "github.com/pluralsh/console-client-go"
+)
+
+type StackRun struct {
+	ID          string
+	Status      gqlclient.StackStatus
+	Type        gqlclient.StackType
+	Tarball     string
+	Steps       []*gqlclient.RunStepFragment
+	Files       []*gqlclient.StackFileFragment
+	Environment []*gqlclient.StackEnvironmentFragment
+}
+
+func (in *StackRun) FromStackRunBaseFragment(fragment *gqlclient.StackRunBaseFragment) *StackRun {
+	return &StackRun{
+		ID:          fragment.ID,
+		Status:      fragment.Status,
+		Type:        fragment.Type,
+		Tarball:     fragment.Tarball,
+		Steps:       fragment.Steps,
+		Files:       fragment.Files,
+		Environment: fragment.Environment,
+	}
+}
+
+// Env parses the StackRun.Environment as a list of strings.
+// Each entry is of the form "key=value".
+func (in *StackRun) Env() []string {
+	result := make([]string, len(in.Environment))
+
+	for i, e := range in.Environment {
+		result[i] = fmt.Sprintf("%s=%s", e.Name, e.Value)
+	}
+
+	return result
+}
