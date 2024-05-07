83cd8979646af9414d60994595f044a2c4fde777
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-05-07 10:38:35 +0200

    add tests for stack agent

--- pkg/controller/stacks/reconciler_test.go
+++ pkg/controller/stacks/reconciler_test.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"time"
 
+	"github.com/Yamashou/gqlgenc/clientv2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	console "github.com/pluralsh/console-client-go"
+	errors2 "github.com/pluralsh/deployment-operator/internal/errors"
 	"github.com/pluralsh/deployment-operator/pkg/controller/stacks"
 	"github.com/pluralsh/deployment-operator/pkg/test/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/vektah/gqlparser/v2/gqlerror"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -31,7 +36,7 @@ var _ = Describe("Stack Run Job Controller", Ordered, func() {
 		job := &batchv1.Job{}
 
 		BeforeAll(func() {
-			By("Creating stack run running job")
+			By("Creating stack run job")
 			err := kClient.Get(ctx, jobNamespacedName, job)
 			if err != nil && errors.IsNotFound(err) {
 				resource := &batchv1.Job{
@@ -58,15 +63,45 @@ var _ = Describe("Stack Run Job Controller", Ordered, func() {
 
 		AfterAll(func() {
 			By("Cleanup stack run job")
-			runningJob := &batchv1.Job{}
-			Expect(kClient.Get(ctx, jobNamespacedName, runningJob)).NotTo(HaveOccurred())
-			Expect(kClient.Delete(ctx, runningJob)).To(Succeed())
+			job := &batchv1.Job{}
+			Expect(kClient.Get(ctx, jobNamespacedName, job)).NotTo(HaveOccurred())
+			Expect(kClient.Delete(ctx, job)).To(Succeed())
 		})
 
-		It("should exit without errors as stack run job is still running", func() {
+		It("should exit without errors as stack run is already deleted", func() {
 			fakeConsoleClient := mocks.NewClientMock(mocks.TestingT)
+			fakeConsoleClient.On("GetStackRun", mock.Anything).Return(nil, &clientv2.ErrorResponse{
+				GqlErrors: &gqlerror.List{gqlerror.Errorf(errors2.ErrorNotFound.String())},
+			})
 
-			reconciler := stacks.NewStackReconciler(fakeConsoleClient, kClient, time.Minute, "", "", "")
+			reconciler := stacks.NewStackReconciler(fakeConsoleClient, kClient, time.Minute, namespace, "", "")
+
+			_, err := reconciler.Reconcile(ctx, stackRunId)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should exit with error as unknown error occurred while getting stack run", func() {
+			fakeConsoleClient := mocks.NewClientMock(mocks.TestingT)
+			fakeConsoleClient.On("GetStackRun", mock.Anything).Return(nil, &clientv2.ErrorResponse{
+				GqlErrors: &gqlerror.List{gqlerror.Errorf("unknown error")},
+			})
+
+			reconciler := stacks.NewStackReconciler(fakeConsoleClient, kClient, time.Minute, namespace, "", "")
+
+			_, err := reconciler.Reconcile(ctx, stackRunId)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("unknown error"))
+		})
+
+		It("should exit without errors as ...", func() {
+			fakeConsoleClient := mocks.NewClientMock(mocks.TestingT)
+			fakeConsoleClient.On("GetStackRun", mock.Anything).Return(&console.StackRunFragment{
+				ID:            stackRunId,
+				Configuration: &console.StackConfigurationFragment{Version: "v1.0.0"},
+				Status:        console.StackStatusSuccessful,
+			}, nil)
+
+			reconciler := stacks.NewStackReconciler(fakeConsoleClient, kClient, time.Minute, namespace, "", "")
 
 			_, err := reconciler.Reconcile(ctx, stackRunId)
 			Expect(err).NotTo(HaveOccurred())
