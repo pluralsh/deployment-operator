b4f9b18e5e8fd773e834b5610cb16102345d8828
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-05-07 13:35:18 +0200

    fix make test

--- pkg/controller/stacks/suite_test.go
+++ pkg/controller/stacks/suite_test.go
@@ -50,7 +50,7 @@ var _ = BeforeSuite(func() {
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: true,
 		// The BinaryAssetsDirectory is only required if you want to run the tests directly
 		// without call the makefile target test. If not informed it will look for the
