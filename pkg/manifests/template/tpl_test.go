78626f7a04ae754c85524119ae3f001e61dc1382
seemywings <kevin.jayne@icloud.com>
2024-05-09 16:52:20 -0400

    readme

--- pkg/manifests/template/tpl_test.go
+++ pkg/manifests/template/tpl_test.go
@@ -3,10 +3,20 @@ package template_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	"github.com/pluralsh/deployment-operator/pkg/manifests/template"
 )
 
 var _ = Describe("Tpl", func() {
+
+	Context("Example Test", func() {
+		It("Should always Pass", func() {
+			Expect(1).To(Equal(1))
+		})
+	})
+
+	Context("Test Should Fail for example output", func() {
+		It("Should always Pass", func() {
+			Expect(1).To(Equal(2))
+		})
+	})
 
 })
