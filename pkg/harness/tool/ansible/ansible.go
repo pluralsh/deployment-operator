8f6f17887a599ea6f175d763ab9e358a7eb19dfc
seemywings <kevin.jayne@icloud.com>
2024-06-04 17:23:22 -0400

    adding harness tool ansible

--- pkg/harness/tool/ansible/ansible.go
+++ pkg/harness/tool/ansible/ansible.go
@@ -1,23 +1,40 @@
 package ansible
 
 import (
-	console "github.com/pluralsh/console-client-go"
+	"context"
+	"os"
 
+	console "github.com/pluralsh/console-client-go"
+	"k8s.io/klog"
+
+	"github.com/pluralsh/deployment-operator/pkg/harness/exec"
 	v1 "github.com/pluralsh/deployment-operator/pkg/harness/tool/v1"
 )
 
-func (in *Ansible) Plan() (*console.StackStateAttributes, error) {
-	// TODO implement me
-	panic("implement me")
+func (in *Ansible) Plan() (string, error) {
+	playbook := os.Getenv("PLAYBOOK")
+	output, err := exec.NewExecutable(
+		"ansible-playbook",
+		exec.WithArgs([]string{playbook, "--check", "--diff"}),
+		exec.WithDir(in.dir),
+	).RunWithOutput(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	klog.V(5).Infoln("Ansible Check was Successful", "output", string(output))
+	return string(output), nil
 }
 
 func (in *Ansible) State() (*console.StackStateAttributes, error) {
-	// TODO implement me
-	panic("implement me")
+	// Ansible doesn't have a state
+	return nil, nil
 }
 
 func (in *Ansible) Output() ([]*console.StackOutputAttributes, error) {
 	// TODO implement me
+	// 1. Run ansible-playbook --diff
+	// 2. Parse the output and return the state
 	panic("implement me")
 }
 
@@ -27,8 +44,8 @@ func (in *Ansible) Modifier(stage console.StepStage) v1.Modifier {
 }
 
 func (in *Ansible) ConfigureStateBackend(actor, deployToken string, urls *console.StackRunBaseFragment_StateUrls) error {
-	//TODO implement me
-	panic("implement me")
+	//Ansible doesn't have a state backend
+	return nil
 }
 
 func New(dir string) *Ansible {
