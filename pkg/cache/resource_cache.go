4e8e6489bae9e69e86befe21488fcb8cf972ceb9
Sebastian Florek <sebastian@plural.sh>
2024-06-27 12:49:44 +0200

    disable health cache

--- pkg/cache/resource_cache.go
+++ pkg/cache/resource_cache.go
@@ -130,7 +130,7 @@ func (in *ResourceCache) watch() {
 func (in *ResourceCache) reconcile(e event.Event) {
 	switch e.Type {
 	case event.ResourceUpdateEvent:
-		GetHealthCache().Update(e.Resource)
+		//GetHealthCache().Update(e.Resource)
 
 		if !in.shouldCacheResource(e.Resource) {
 			in.deleteCacheEntry(e.Resource)
