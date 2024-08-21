8ad57f1f3df86c256cf289302ea10600e03ec358
Sebastian Florek <sebastian@plural.sh>
2024-08-21 11:20:56 +0200

    use same golangci-lint version in the CI and Makefile target

--- tools.go
+++ tools.go
@@ -6,4 +6,5 @@ import (
 	_ "github.com/elastic/crd-ref-docs"
 	_ "github.com/vektra/mockery/v2"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 )
