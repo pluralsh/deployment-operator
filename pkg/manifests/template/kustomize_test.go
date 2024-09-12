0b69880eee71a3db5011193d0635e16bb0dd9168
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-09-12 17:57:49 +0200

    feat: kustomize services with liquid templating (#258)
    * kustomize services with liquid templating
    
    * fix linter
    
    * create a new file
    
    * code review

--- pkg/manifests/template/kustomize_test.go
+++ pkg/manifests/template/kustomize_test.go
@@ -1,6 +1,7 @@
 package template
 
 import (
+	"os"
 	"path/filepath"
 	"sort"
 	"strings"
@@ -30,6 +31,50 @@ var _ = Describe("Kustomize template", func() {
 			})
 			Expect(resp[0].GetKind()).To(Equal("ConfigMap"))
 			Expect(strings.HasPrefix(resp[0].GetName(), "app-config")).Should(BeTrue())
+			Expect(resp[1].GetKind()).To(Equal("Deployment"))
+			Expect(resp[2].GetKind()).To(Equal("Secret"))
+			Expect(strings.HasPrefix(resp[2].GetName(), "credentials")).Should(BeTrue())
+		})
+
+	})
+})
+
+var _ = Describe("Kustomize liquid template", func() {
+	const (
+		name = "test"
+	)
+
+	dir := filepath.Join("..", "..", "..", "test", "kustomize", "liquid")
+	AfterEach(func() {
+		Expect(os.Remove(filepath.Join(dir, "dev", "kustomization.yaml"))).To(Succeed())
+	})
+	svc := &console.GetServiceDeploymentForAgent_ServiceDeployment{
+		Namespace: "default",
+		Kustomize: &console.GetServiceDeploymentForAgent_ServiceDeployment_Kustomize{
+			Path: "",
+		},
+	}
+	Context("Render kustomize liquid template", func() {
+		It("should successfully render the liquid template", func() {
+			svc.Kustomize.Path = "dev"
+			svc.Configuration = []*console.GetServiceDeploymentForAgent_ServiceDeployment_Configuration{
+				{
+					Name:  "name",
+					Value: name,
+				},
+			}
+			svc.Cluster = &console.GetServiceDeploymentForAgent_ServiceDeployment_Cluster{
+				ID:   "123",
+				Name: "test",
+			}
+			resp, err := NewKustomize(dir).Render(svc, utilFactory)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(resp)).To(Equal(3))
+			sort.Slice(resp, func(i, j int) bool {
+				return resp[i].GetKind() < resp[j].GetKind()
+			})
+			Expect(resp[0].GetKind()).To(Equal("ConfigMap"))
+			Expect(strings.HasPrefix(resp[0].GetName(), name)).Should(BeTrue())
 			Expect(resp[1].GetKind()).To(Equal("Deployment"))
 			Expect(resp[2].GetKind()).To(Equal("Secret"))
 			Expect(strings.HasPrefix(resp[2].GetName(), "credentials")).Should(BeTrue())
