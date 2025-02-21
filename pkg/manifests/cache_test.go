65f85a8edcab7c324ba9a5d4ef375922afdb6e69
John Blackwell <john.blackwell@treno.io>
2025-02-21 14:04:17 -0500

    update UT to mock api better

--- pkg/manifests/cache_test.go
+++ pkg/manifests/cache_test.go
@@ -8,15 +8,15 @@ import (
 
 func TestBuildTarballURL(t *testing.T) {
 	t.Run("valid URL, no SHA", func(t *testing.T) {
-		u, err := buildTarballURL("https://example.com/foo/bar", "")
+		u, err := buildTarballURL("https://example.com/foo/bar?serviceId=some-id", "")
 		require.NoError(t, err)
-		require.Equal(t, "https://example.com/foo/bar", u.String())
+		require.Equal(t, "https://example.com/foo/bar?serviceId=some-id", u.String())
 	})
 
 	t.Run("valid URL with SHA", func(t *testing.T) {
-		u, err := buildTarballURL("https://example.com/foo/bar", "abc123")
+		u, err := buildTarballURL("https://example.com/foo/bar?serviceId=some-id", "abc123")
 		require.NoError(t, err)
-		require.Equal(t, "https://example.com/foo/bar?digest=abc123", u.String())
+		require.Equal(t, "https://example.com/foo/bar?digest=abc123&serviceId=some-id", u.String())
 	})
 
 	t.Run("invalid URL", func(t *testing.T) {
