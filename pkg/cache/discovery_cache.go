af76d7902ecc19fcb2952c75bd0bad2c56f0969f
Sebastian Florek <sebastian@plural.sh>
2025-03-10 13:28:19 +0100

    discover service mesh installation through discovery API groups

--- pkg/cache/discovery_cache.go
+++ pkg/cache/discovery_cache.go
@@ -3,19 +3,40 @@ package cache
 import (
 	"context"
 	"fmt"
+	"strings"
+	"sync"
 	"time"
 
 	cmap "github.com/orcaman/concurrent-map/v2"
-	"github.com/pluralsh/deployment-operator/internal/helpers"
-	"github.com/pluralsh/deployment-operator/internal/metrics"
+	"github.com/pluralsh/console/go/client"
+	"github.com/samber/lo"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
 	"k8s.io/klog/v2"
+
+	"github.com/pluralsh/deployment-operator/internal/helpers"
+	"github.com/pluralsh/deployment-operator/internal/metrics"
+)
+
+const (
+	// ServiceMeshResourceGroupIstio is a base group name used by Istio
+	// Ref: https://github.com/istio/istio/blob/6186a80cb220ecbd7e1cc82044fe3a6fc2876c63/operator/pkg/apis/register.go#L27-L31
+	ServiceMeshResourceGroupIstio string = "istio.io"
+
+	// ServiceMeshResourceGroupCilium is a base group name used by Cilium
+	// Ref: https://github.com/cilium/cilium/blob/99b4bc0d0b628f22c024f3ea74ef21007a831f52/pkg/k8s/apis/cilium.io/register.go#L7-L8
+	ServiceMeshResourceGroupCilium string = "cilium.io"
+
+	// ServiceMeshResourceGroupLinkerd is a base group name used by Linkerd
+	// Ref: https://github.com/linkerd/linkerd2/blob/e055c32f31ae7618281fed1eb5c304b0d1389a52/controller/gen/apis/serviceprofile/register.go#L3-L4
+	ServiceMeshResourceGroupLinkerd string = "linkerd.io"
 )
 
 var (
 	// Maps a GroupVersion to resource Kind.
-	discoveryCache cmap.ConcurrentMap[string, bool]
+	discoveryCache    cmap.ConcurrentMap[string, bool]
+	serviceMesh       = ""
+	serviceMeshRWLock = sync.RWMutex{}
 )
 
 func init() {
@@ -24,6 +45,22 @@ func init() {
 
 func DiscoveryCache() cmap.ConcurrentMap[string, bool] {
 	return discoveryCache
+}
+
+func ServiceMesh() *client.ServiceMesh {
+	serviceMeshRWLock.RLock()
+	defer serviceMeshRWLock.RUnlock()
+
+	switch serviceMesh {
+	case ServiceMeshResourceGroupIstio:
+		return lo.ToPtr(client.ServiceMeshIstio)
+	case ServiceMeshResourceGroupCilium:
+		return lo.ToPtr(client.ServiceMeshCilium)
+	case ServiceMeshResourceGroupLinkerd:
+		return lo.ToPtr(client.ServiceMeshLinkerd)
+	default:
+		return nil
+	}
 }
 
 func RunDiscoveryCacheInBackgroundOrDie(ctx context.Context, discoveryClient *discovery.DiscoveryClient) {
@@ -61,8 +98,26 @@ func updateDiscoveryCache(discoveryClient *discovery.DiscoveryClient) error {
 
 			discoveryCache.Set(fmt.Sprintf("%s/%s", gv.String(), resource.Kind), true)
 			discoveryCache.Set(gv.String(), true)
+
+			checkAndUpdateServiceMesh(gv.Group)
 		}
 	}
 
 	return err
+}
+
+func checkAndUpdateServiceMesh(group string) {
+	serviceMeshRWLock.Lock()
+	defer serviceMeshRWLock.Unlock()
+
+	switch {
+	case strings.Contains(group, ServiceMeshResourceGroupIstio):
+		serviceMesh = ServiceMeshResourceGroupIstio
+		break
+	case strings.Contains(group, ServiceMeshResourceGroupCilium):
+		serviceMesh = ServiceMeshResourceGroupCilium
+		break
+	case strings.Contains(group, ServiceMeshResourceGroupLinkerd):
+		serviceMesh = ServiceMeshResourceGroupLinkerd
+	}
 }
