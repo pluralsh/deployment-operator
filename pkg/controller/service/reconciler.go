6749e09d93b057f1a34e5ed8ece6fd39bd2119fa
Lukasz Zajaczkowski <zreigz@gmail.com>
2025-03-12 11:16:41 +0100

    expire svc cache

--- pkg/controller/service/reconciler.go
+++ pkg/controller/service/reconciler.go
@@ -364,6 +364,7 @@ func (s *ServiceReconciler) Reconcile(ctx context.Context, id string) (result re
 		})
 
 		metrics.Record().ServiceDeletion(id)
+		s.svcCache.Expire(id)
 		s.manifestCache.Expire(id)
 		err = s.UpdatePruneStatus(ctx, svc, ch, map[schema.GroupName]string{})
 		return
