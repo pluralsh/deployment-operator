1d5bc24f07c77900cd7a05c95951970fbdde83bc
Lukasz Zajaczkowski <zreigz@gmail.com>
2025-03-21 09:26:32 +0100

    linter

--- internal/utils/kubernetes.go
+++ internal/utils/kubernetes.go
@@ -58,7 +58,7 @@ func TryToUpdate(ctx context.Context, client ctrlruntimeclient.Client, object ct
 	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		original := object.DeepCopyObject().(ctrlruntimeclient.Object)
 		if err := client.Get(ctx, key, object); err != nil {
-			return fmt.Errorf("could not fetch current %s/%s state, got error: %+v", object.GetName(), object.GetNamespace(), err)
+			return fmt.Errorf("could not fetch current %s/%s state, got error: %w", object.GetName(), object.GetNamespace(), err)
 		}
 
 		if reflect.DeepEqual(object, original) {
