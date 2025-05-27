8a1e8c2d4f58fbb7011f7d96f147b3e6ac2460ba
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-05-27 12:38:50 +0200

    format

--- pkg/cache/db/queries.go
+++ pkg/cache/db/queries.go
@@ -97,9 +97,9 @@ const (
 	`
 
 	nodeStatistics = `
-	SELECT node, COUNT(*)
-	FROM Pod
-	WHERE datetime(createdAt) < datetime('now', '-5 minutes')
-	GROUP BY node
+		SELECT node, COUNT(*)
+		FROM Pod
+		WHERE DATETIME(createdAt) < DATETIME('now', '-5 minutes')
+		GROUP BY node
 	`
 )
