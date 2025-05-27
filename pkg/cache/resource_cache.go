e92c8aaac71f92dfdd4546063d222519fbfd94bf
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-05-27 12:50:47 +0200

    watch pods

--- pkg/cache/resource_cache.go
+++ pkg/cache/resource_cache.go
@@ -33,6 +33,7 @@ import (
 
 var allCoreObjMetadata = containers.ToSet([]ResourceKey{
 	// Core API group ("")
+	{GroupKind: runtimeschema.GroupKind{Group: "", Kind: "Pod"}, Name: resourceKeyPlaceholder, Namespace: resourceKeyPlaceholder},
 	{GroupKind: runtimeschema.GroupKind{Group: "", Kind: "Service"}, Name: resourceKeyPlaceholder, Namespace: resourceKeyPlaceholder},
 	{GroupKind: runtimeschema.GroupKind{Group: "", Kind: "ConfigMap"}, Name: resourceKeyPlaceholder, Namespace: resourceKeyPlaceholder},
 	{GroupKind: runtimeschema.GroupKind{Group: "", Kind: "Secret"}, Name: resourceKeyPlaceholder, Namespace: resourceKeyPlaceholder},
@@ -299,6 +300,7 @@ func (in *ResourceCache) reconcile(e event.Event) {
 		return
 	}
 
+	//todo
 	if e.Resource == nil {
 		return
 	}
