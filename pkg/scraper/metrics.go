73d9c49cbac8ba97fde2cc84cd8b79cdb12b3b25
michaeljguarino <mjg@plural.sh>
2025-07-27 16:00:49 -0400

    Minor fixes to node utilization scraping
    Need to use memory/cpu available metrics, and fall back to gathering info from nodes if metrics server is not found

--- pkg/scraper/metrics.go
+++ pkg/scraper/metrics.go
@@ -6,11 +6,13 @@ import (
 	"sync"
 	"time"
 
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
 	"github.com/pluralsh/deployment-operator/api/v1alpha1"
 	"github.com/pluralsh/deployment-operator/internal/helpers"
 	"github.com/pluralsh/deployment-operator/pkg/common"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/discovery"
 	"k8s.io/klog/v2"
 	metricsclientset "k8s.io/metrics/pkg/client/clientset/versioned"
@@ -20,18 +22,21 @@ import (
 const name = "metrics scraper"
 
 var (
-	metrics *Metrics
+	metrics         *Metrics
+	nodeCacheExpiry = 30 * time.Minute
 )
 
 func init() {
 	metrics = &Metrics{
-		metrics: v1alpha1.MetricsAggregateStatus{},
+		metrics:     v1alpha1.MetricsAggregateStatus{},
+		lastUpdated: time.Now().Add(-nodeCacheExpiry),
 	}
 }
 
 type Metrics struct {
-	mu      sync.RWMutex
-	metrics v1alpha1.MetricsAggregateStatus
+	mu          sync.RWMutex
+	metrics     v1alpha1.MetricsAggregateStatus
+	lastUpdated time.Time
 }
 
 func GetMetrics() *Metrics {
@@ -42,6 +47,7 @@ func (s *Metrics) Add(metrics v1alpha1.MetricsAggregateStatus) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.metrics = metrics
+	s.lastUpdated = time.Now()
 }
 
 func (s *Metrics) Get() v1alpha1.MetricsAggregateStatus {
@@ -66,7 +72,13 @@ func RunMetricsScraperInBackgroundOrDie(ctx context.Context, k8sClient ctrclient
 			if err == nil && status != nil {
 				GetMetrics().Add(*status)
 			} else if err != nil {
-				klog.Error(err, "failed to get metrics")
+				if time.Since(GetMetrics().lastUpdated) > nodeCacheExpiry {
+					if cs, err := kubernetes.NewForConfig(config); err == nil {
+						getMetricsFromNodes(ctx, cs)
+						return false, nil
+					}
+					klog.ErrorS(err, "failed to get metrics from nodes")
+				}
 			}
 		}
 		return false, nil
@@ -74,4 +86,21 @@ func RunMetricsScraperInBackgroundOrDie(ctx context.Context, k8sClient ctrclient
 	if err != nil {
 		panic(fmt.Errorf("failed to start %s in background: %w", name, err))
 	}
+}
+
+func getMetricsFromNodes(ctx context.Context, client *kubernetes.Clientset) {
+	nodes, err := client.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		klog.ErrorS(err, "failed to list nodes")
+		return
+	}
+
+	status := v1alpha1.MetricsAggregateStatus{}
+	for _, node := range nodes.Items {
+		status.MemoryTotalBytes += node.Status.Allocatable.Memory().Value()
+		status.CPUTotalMillicores += node.Status.Allocatable.Cpu().MilliValue()
+		status.MemoryAvailableBytes += node.Status.Capacity.Memory().Value()
+		status.CPUAvailableMillicores += node.Status.Capacity.Cpu().MilliValue()
+	}
+	GetMetrics().Add(status)
 }
