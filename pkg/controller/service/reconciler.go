f8e4af0d16036b6f213191ef3211e260eeaff5fd
Lukasz Zajaczkowski <zreigz@gmail.com>
2025-08-12 15:01:58 +0200

    fix destroyer

--- pkg/controller/service/reconciler.go
+++ pkg/controller/service/reconciler.go
@@ -161,10 +161,7 @@ func newApplier(invFactory inventory.ClientFactory, f util.Factory) (*applier.Ap
 }
 
 func newDestroyer(invFactory inventory.ClientFactory, f util.Factory) (*apply.Destroyer, error) {
-	invClient, err := invFactory.NewClient(f)
-	if err != nil {
-		return nil, err
-	}
+	invClient := &db.CacheInventoryClient{}
 
 	return apply.NewDestroyerBuilder().
 		WithFactory(f).
@@ -423,8 +420,13 @@ func (s *ServiceReconciler) Reconcile(ctx context.Context, id string) (result re
 	logger.V(2).Info("syncing service", "name", svc.Name, "namespace", svc.Namespace)
 
 	if svc.DeletedAt != nil {
+		var inv inventory.Info
 		logger.V(2).Info("Deleting service", "name", svc.Name, "namespace", svc.Namespace)
-		ch := s.destroyer.Run(ctx, inventory.WrapInventoryInfoObj(lo.ToPtr(s.defaultInventoryObjTemplate(id))), apply.DestroyerOptions{
+		inv, err = db.RegisterInventory(ctx, id, svc.Namespace)
+		if err != nil {
+			return
+		}
+		ch := s.destroyer.Run(ctx, inv, apply.DestroyerOptions{
 			InventoryPolicy:         inventory.PolicyAdoptIfNoInventory,
 			DryRunStrategy:          common.DryRunNone,
 			DeleteTimeout:           20 * time.Second,
@@ -552,45 +554,6 @@ func (s *ServiceReconciler) CheckNamespace(namespace string, syncConfig *console
 		return utils.CheckNamespace(*s.clientset, namespace, labels, annotations)
 	}
 	return nil
-}
-
-func (s *ServiceReconciler) SplitObjects(id string, objs []unstructured.Unstructured) (unstructured.Unstructured, []unstructured.Unstructured, error) {
-	invs := make([]unstructured.Unstructured, 0, 1)
-	resources := make([]unstructured.Unstructured, 0, len(objs))
-	for _, obj := range objs {
-		if inventory.IsInventoryObject(&obj) {
-			invs = append(invs, obj)
-		} else {
-			resources = append(resources, obj)
-		}
-	}
-	switch len(invs) {
-	case 0:
-		return s.defaultInventoryObjTemplate(id), resources, nil
-	case 1:
-		return invs[0], resources, nil
-	default:
-		return unstructured.Unstructured{}, nil, fmt.Errorf("expecting zero or one inventory object, found %d", len(invs))
-	}
-}
-
-func (s *ServiceReconciler) defaultInventoryObjTemplate(id string) unstructured.Unstructured {
-	name := "inventory-" + id
-	namespace := "plrl-deploy-operator"
-
-	return unstructured.Unstructured{
-		Object: map[string]interface{}{
-			"apiVersion": "v1",
-			"kind":       "ConfigMap",
-			"metadata": map[string]interface{}{
-				"name":      name,
-				"namespace": namespace,
-				"labels": map[string]interface{}{
-					common.InventoryLabel: id,
-				},
-			},
-		},
-	}
 }
 
 func (s *ServiceReconciler) isClusterRestore(ctx context.Context) (bool, error) {
