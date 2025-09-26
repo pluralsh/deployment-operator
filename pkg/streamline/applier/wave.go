25e279ed8b9b941d498f96877c6875c8b7d673d5
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-09-26 10:15:41 +0200

    move and integrate the default wave logic into the `GetSyncWave` function for resource ordering

--- pkg/streamline/applier/wave.go
+++ pkg/streamline/applier/wave.go
@@ -3,11 +3,13 @@ package applier
 import (
 	"context"
 	"fmt"
+	"slices"
 	"sync"
 	"time"
 
 	"github.com/pluralsh/deployment-operator/internal/utils"
 	"github.com/pluralsh/deployment-operator/pkg/manifests/template"
+	"github.com/pluralsh/polly/algorithms"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -70,67 +72,26 @@ func (in *Wave) Len() int {
 type Waves []Wave
 
 func NewWaves(resources []unstructured.Unstructured) Waves {
-	defaultWaveCount := 5
-	waves := make([]Wave, 0, defaultWaveCount)
-	for i := 0; i < defaultWaveCount; i++ {
-		waves = append(waves, NewWave([]unstructured.Unstructured{}, ApplyWave))
-	}
-
-	kindToWave := map[string]int{
-		// Wave 0 - core non-namespaced resources
-		common.NamespaceKind:                0,
-		common.CustomResourceDefinitionKind: 0,
-		common.PersistentVolumeKind:         0,
-		common.ClusterRoleKind:              0,
-		common.ClusterRoleListKind:          0,
-		common.ClusterRoleBindingKind:       0,
-		common.ClusterRoleBindingListKind:   0,
-		common.StorageClassKind:             0,
-
-		// Wave 1 - core namespaced configuration resources
-		common.ConfigMapKind:             1,
-		common.SecretKind:                1,
-		common.SecretListKind:            1,
-		common.ServiceAccountKind:        1,
-		common.RoleKind:                  1,
-		common.RoleListKind:              1,
-		common.RoleBindingKind:           1,
-		common.RoleBindingListKind:       1,
-		common.PodDisruptionBudgetKind:   1,
-		common.ResourceQuotaKind:         1,
-		common.NetworkPolicyKind:         1,
-		common.LimitRangeKind:            1,
-		common.PodSecurityPolicyKind:     1,
-		common.IngressClassKind:          1,
-		common.PersistentVolumeClaimKind: 1,
-
-		// Wave 2 - core namespaced workload resources
-		common.DeploymentKind:            2,
-		common.DaemonSetKind:             2,
-		common.StatefulSetKind:           2,
-		common.ReplicaSetKind:            2,
-		common.JobKind:                   2,
-		common.CronJobKind:               2,
-		common.PodKind:                   2,
-		common.ReplicationControllerKind: 2,
-
-		// Wave 3 - core namespaced networking resources
-		common.EndpointsKind:  3,
-		common.ServiceKind:    3,
-		common.IngressKind:    3,
-		common.APIServiceKind: 3,
-	}
-
+	wavesMap := make(map[int]Wave)
 	for _, resource := range resources {
-		if waveIdx, exists := kindToWave[resource.GetKind()]; exists {
-			waves[waveIdx].Add(resource)
+		i := smcommon.GetSyncWave(resource)
+		if wave, ok := wavesMap[i]; !ok {
+			wavesMap[i] = NewWave([]unstructured.Unstructured{resource}, ApplyWave)
 		} else {
-			// Unknown resource kind, put it in the last wave
-			waves[len(waves)-1].Add(resource)
+			wave.Add(resource)
+			wavesMap[i] = wave
 		}
 	}
 
-	return waves
+	waves := lo.Entries(wavesMap)
+
+	slices.SortFunc(waves, func(a, b lo.Entry[int, Wave]) int {
+		return a.Key - b.Key
+	})
+
+	return algorithms.Map(waves, func(e lo.Entry[int, Wave]) Wave {
+		return e.Value
+	})
 }
 
 const (
--- pkg/streamline/common/const.go
+++ pkg/streamline/common/const.go
@@ -3,6 +3,7 @@ package common
 import (
 	"strconv"
 
+	"github.com/pluralsh/deployment-operator/pkg/common"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -32,6 +33,55 @@ const (
 
 	// SyncWaveAnnotation allows users to customize resource apply ordering when needed.
 	SyncWaveAnnotation = "deployment.plural.sh/sync-wave"
+)
+
+var (
+	kindToDefaultSyncWave = map[string]int{
+		// Wave 0 - core non-namespaced resources
+		common.NamespaceKind:                0,
+		common.CustomResourceDefinitionKind: 0,
+		common.PersistentVolumeKind:         0,
+		common.ClusterRoleKind:              0,
+		common.ClusterRoleListKind:          0,
+		common.ClusterRoleBindingKind:       0,
+		common.ClusterRoleBindingListKind:   0,
+		common.StorageClassKind:             0,
+
+		// Wave 1 - core namespaced configuration resources
+		common.ConfigMapKind:             1,
+		common.SecretKind:                1,
+		common.SecretListKind:            1,
+		common.ServiceAccountKind:        1,
+		common.RoleKind:                  1,
+		common.RoleListKind:              1,
+		common.RoleBindingKind:           1,
+		common.RoleBindingListKind:       1,
+		common.PodDisruptionBudgetKind:   1,
+		common.ResourceQuotaKind:         1,
+		common.NetworkPolicyKind:         1,
+		common.LimitRangeKind:            1,
+		common.PodSecurityPolicyKind:     1,
+		common.IngressClassKind:          1,
+		common.PersistentVolumeClaimKind: 1,
+
+		// Wave 2 - core namespaced workload resources
+		common.DeploymentKind:            2,
+		common.DaemonSetKind:             2,
+		common.StatefulSetKind:           2,
+		common.ReplicaSetKind:            2,
+		common.JobKind:                   2,
+		common.CronJobKind:               2,
+		common.PodKind:                   2,
+		common.ReplicationControllerKind: 2,
+
+		// Wave 3 - core namespaced networking resources
+		common.EndpointsKind:  3,
+		common.ServiceKind:    3,
+		common.IngressKind:    3,
+		common.APIServiceKind: 3,
+	}
+
+	lastDefaultWave = 3 // TODO
 )
 
 func GetOwningInventory(obj unstructured.Unstructured) string {
@@ -66,21 +116,32 @@ func ValidateTrackingIdentifier(u unstructured.Unstructured) bool {
 
 // GetSyncWave retrieves the sync wave from the resource annotations.
 // If the annotation is not present or invalid, it returns nil.
-func GetSyncWave(obj unstructured.Unstructured) *int {
-	annotations := obj.GetAnnotations()
+func GetSyncWave(u unstructured.Unstructured) int {
+	annotations := u.GetAnnotations()
 	if annotations == nil {
-		return nil
+		return defaultWave(u)
 	}
 
 	wave, ok := annotations[SyncWaveAnnotation]
 	if !ok {
-		return nil
+		return defaultWave(u)
 	}
 
-	waveInt, err := strconv.Atoi(wave)
+	i, err := strconv.Atoi(wave)
 	if err != nil {
-		return nil
+		return defaultWave(u)
 	}
 
-	return &waveInt
+	return i
+}
+
+// defaultWave returns default sync wave for a resource based on its kind.
+// If the sync wave was not defined for a kind, it returns the last default wave.
+func defaultWave(u unstructured.Unstructured) int {
+	i, ok := kindToDefaultSyncWave[u.GetKind()]
+	if !ok {
+		return lastDefaultWave
+	}
+
+	return i
 }
