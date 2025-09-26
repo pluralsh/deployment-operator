5d8f5d787ef42515ca54eeadcb5eeed2b0885f2a
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-09-26 12:52:04 +0200

    add skipped resource tracking and integrate skip/delete filters into phase construction

--- pkg/streamline/applier/phase.go
+++ pkg/streamline/applier/phase.go
@@ -10,12 +10,17 @@ import (
 )
 
 type Phase struct {
-	name  smcommon.SyncPhase
-	waves []Wave
+	name    smcommon.SyncPhase
+	skipped []unstructured.Unstructured
+	waves   []Wave
 }
 
 func (p *Phase) Name() smcommon.SyncPhase {
 	return p.name
+}
+
+func (p *Phase) Skipped() []unstructured.Unstructured {
+	return p.skipped
 }
 
 func (p *Phase) Waves() []Wave {
@@ -26,9 +31,22 @@ func (p *Phase) AddWave(wave Wave) {
 	p.waves = append(p.waves, wave)
 }
 
-func NewPhase(name smcommon.SyncPhase, resources []unstructured.Unstructured) Phase {
+func NewPhase(name smcommon.SyncPhase, resources []unstructured.Unstructured, skipFilter, deleteFilter FilterFunc) Phase {
+	skipped := make([]unstructured.Unstructured, 0)
+	deleteWave := NewWave([]unstructured.Unstructured{}, DeleteWave)
+
 	wavesMap := make(map[int]Wave)
 	for _, resource := range resources {
+		if deleteFilter(resource) {
+			deleteWave.Add(resource)
+			continue
+		}
+
+		if skipFilter(resource) {
+			skipped = append(skipped, resource)
+			continue
+		}
+
 		i := smcommon.GetSyncWave(resource)
 		if wave, ok := wavesMap[i]; !ok {
 			wavesMap[i] = NewWave([]unstructured.Unstructured{resource}, ApplyWave)
@@ -44,10 +62,29 @@ func NewPhase(name smcommon.SyncPhase, resources []unstructured.Unstructured) Ph
 		return a.Key - b.Key
 	})
 
-	return Phase{
-		name: name,
-		waves: algorithms.Map(waves, func(e lo.Entry[int, Wave]) Wave {
-			return e.Value
-		}),
+	phase := Phase{
+		name:    name,
+		skipped: skipped,
+		waves:   algorithms.Map(waves, func(e lo.Entry[int, Wave]) Wave { return e.Value }),
 	}
+
+	phase.AddWave(deleteWave)
+
+	return phase
+}
+
+func NewPhases(resources []unstructured.Unstructured, skipFilter, deleteFilter FilterFunc) map[smcommon.SyncPhase]Phase {
+	phases := make(map[smcommon.SyncPhase][]unstructured.Unstructured)
+	for _, resource := range resources {
+		p := smcommon.GetSyncPhase(resource)
+		if phase, ok := phases[p]; !ok {
+			phases[p] = []unstructured.Unstructured{resource}
+		} else {
+			phases[p] = append(phase, resource)
+		}
+	}
+
+	return lo.MapValues(phases, func(u []unstructured.Unstructured, p smcommon.SyncPhase) Phase {
+		return NewPhase(p, u, skipFilter, deleteFilter)
+	})
 }
