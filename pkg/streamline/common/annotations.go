d87a04f54fa2abba15c4fb21fc941014388584ad
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-09-29 10:16:12 +0200

    rename `kindToDefaultSyncWave` to `kindSyncPriorities` and replace `lastDefaultWave` with `defaultSyncPriority`

--- pkg/streamline/common/annotations.go
+++ pkg/streamline/common/annotations.go
@@ -49,56 +49,55 @@ const (
 	SyncPhasePostSync SyncPhase = "post-sync"
 	SyncPhaseSyncFail SyncPhase = "sync-fail"
 	SyncPhaseSkip     SyncPhase = "skip"
+
+	// defaultSyncPriority should be after the last priority from kindSyncPriorities.
+	defaultSyncPriority = 4
 )
 
-var (
-	kindToDefaultSyncWave = map[string]int{
-		// Wave 0 - core non-namespaced resources
-		common.NamespaceKind:                0,
-		common.CustomResourceDefinitionKind: 0,
-		common.PersistentVolumeKind:         0,
-		common.ClusterRoleKind:              0,
-		common.ClusterRoleListKind:          0,
-		common.ClusterRoleBindingKind:       0,
-		common.ClusterRoleBindingListKind:   0,
-		common.StorageClassKind:             0,
+var kindSyncPriorities = map[string]int{
+	// Wave 0 - core non-namespaced resources
+	common.NamespaceKind:                0,
+	common.CustomResourceDefinitionKind: 0,
+	common.PersistentVolumeKind:         0,
+	common.ClusterRoleKind:              0,
+	common.ClusterRoleListKind:          0,
+	common.ClusterRoleBindingKind:       0,
+	common.ClusterRoleBindingListKind:   0,
+	common.StorageClassKind:             0,
 
-		// Wave 1 - core namespaced configuration resources
-		common.ConfigMapKind:             1,
-		common.SecretKind:                1,
-		common.SecretListKind:            1,
-		common.ServiceAccountKind:        1,
-		common.RoleKind:                  1,
-		common.RoleListKind:              1,
-		common.RoleBindingKind:           1,
-		common.RoleBindingListKind:       1,
-		common.PodDisruptionBudgetKind:   1,
-		common.ResourceQuotaKind:         1,
-		common.NetworkPolicyKind:         1,
-		common.LimitRangeKind:            1,
-		common.PodSecurityPolicyKind:     1,
-		common.IngressClassKind:          1,
-		common.PersistentVolumeClaimKind: 1,
+	// Wave 1 - core namespaced configuration resources
+	common.ConfigMapKind:             1,
+	common.SecretKind:                1,
+	common.SecretListKind:            1,
+	common.ServiceAccountKind:        1,
+	common.RoleKind:                  1,
+	common.RoleListKind:              1,
+	common.RoleBindingKind:           1,
+	common.RoleBindingListKind:       1,
+	common.PodDisruptionBudgetKind:   1,
+	common.ResourceQuotaKind:         1,
+	common.NetworkPolicyKind:         1,
+	common.LimitRangeKind:            1,
+	common.PodSecurityPolicyKind:     1,
+	common.IngressClassKind:          1,
+	common.PersistentVolumeClaimKind: 1,
 
-		// Wave 2 - core namespaced workload resources
-		common.DeploymentKind:            2,
-		common.DaemonSetKind:             2,
-		common.StatefulSetKind:           2,
-		common.ReplicaSetKind:            2,
-		common.JobKind:                   2,
-		common.CronJobKind:               2,
-		common.PodKind:                   2,
-		common.ReplicationControllerKind: 2,
+	// Wave 2 - core namespaced workload resources
+	common.DeploymentKind:            2,
+	common.DaemonSetKind:             2,
+	common.StatefulSetKind:           2,
+	common.ReplicaSetKind:            2,
+	common.JobKind:                   2,
+	common.CronJobKind:               2,
+	common.PodKind:                   2,
+	common.ReplicationControllerKind: 2,
 
-		// Wave 3 - core namespaced networking resources
-		common.EndpointsKind:  3,
-		common.ServiceKind:    3,
-		common.IngressKind:    3,
-		common.APIServiceKind: 3,
-	}
-
-	lastDefaultWave = 3 // TODO
-)
+	// Wave 3 - core namespaced networking resources
+	common.EndpointsKind:  3,
+	common.ServiceKind:    3,
+	common.IngressKind:    3,
+	common.APIServiceKind: 3,
+}
 
 func GetOwningInventory(obj unstructured.Unstructured) string {
 	annotations := obj.GetAnnotations()
@@ -154,9 +153,9 @@ func GetSyncWave(u unstructured.Unstructured) int {
 // defaultWave returns default sync wave for a resource based on its kind.
 // If the sync wave was not defined for a kind, it returns the last default wave.
 func defaultWave(u unstructured.Unstructured) int {
-	i, ok := kindToDefaultSyncWave[u.GetKind()]
+	i, ok := kindSyncPriorities[u.GetKind()]
 	if !ok {
-		return lastDefaultWave
+		return defaultSyncPriority
 	}
 
 	return i
