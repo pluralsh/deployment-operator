2d801e7a35e2d36a3efb41e24cdc82787ffea0a1
JohnBlackwell <john@plural.sh>
2025-10-02 15:01:31 -0400

    log images extracted

--- pkg/controller/service/reconciler.go
+++ pkg/controller/service/reconciler.go
@@ -524,6 +524,14 @@ func (s *ServiceReconciler) Reconcile(ctx context.Context, id string) (result re
 
 	metadata := s.ExtractImagesMetadata(appliedResources)
 
+	if metadata.Images != nil {
+		if len(metadata.Images) > 0 {
+			logger.Info("extracted images from metadata in service", "name", svc.Name, "namespace", svc.Namespace, "images", metadata.Images)
+		} else {
+			logger.Info("didnt extract any images from service", "name", svc.Name, "namespace", svc.Namespace)
+		}
+	}
+
 	if err = s.UpdateStatus(svc.ID, svc.Revision.ID, svc.Sha, lo.ToSlicePtr(components), lo.ToSlicePtr(errs), metadata); err != nil {
 		logger.Error(err, "Failed to update service status, ignoring for now")
 	} else {
