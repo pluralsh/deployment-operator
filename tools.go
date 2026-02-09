844caf8b03a9ea50430b64b0982ce45cd8878bd8
Sebastian Florek <s.florek91@gmail.com>
2026-02-09 13:42:00 +0100

    **chore(deployment-operator): update golangci-lint tool to latest version

--- tools.go
+++ tools.go
@@ -4,7 +4,7 @@ package tools
 
 import (
 	_ "github.com/elastic/crd-ref-docs"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/vektra/mockery/v2"
 	_ "gotest.tools/gotestsum"
 	_ "k8s.io/client-go/discovery"
